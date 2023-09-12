603e5ac61c8231b72fe06c9a52948cd5066cf7de
AIFuzi <39236252+AIFuzi@users.noreply.github.com>
2023-09-12 14:07:30 +0300

    Todo project test

--- src/pages/Todolist.jsx
+++ src/pages/Todolist.jsx
@@ -1,31 +1,74 @@
-import { Button } from 'antd';
-import React, { useContext, useEffect } from 'react';
+import { Avatar, Button, Menu } from 'antd';
+import React, { useContext, useEffect, useState } from 'react';
 import { AuthContext } from '../context/context';
 import { invoke } from '@tauri-apps/api';
+import {
+  PieChartOutlined,
+  PlusOutlined,
+  UserOutlined,
+} from '@ant-design/icons';
+import todoStyle from '../styles/todo.module.css';
+import Title from 'antd/es/typography/Title';
+import Link from 'antd/es/typography/Link';
 
 const Todolist = () => {
   const { setIsAuth } = useContext(AuthContext);
+  const [index, setIndex] = useState(1);
+  const [items, setItems] = useState([]);
 
   const logout = async () => {
-    // setIsAuth(false);
-    // localStorage.removeItem('tok');
-
-    console.log(localStorage.getItem('us_lg'));
     await invoke('logout_user', { token: localStorage.getItem('tok') });
-    localStorage.removeItem('tok');
+    localStorage.clear();
     setIsAuth(false);
   };
 
-  useEffect(() => {
-    if (localStorage.getItem('tok')) {
-      setIsAuth(true);
-    }
-  }, []);
+  const createProject = () => {
+    setIndex(index + 1);
+    setItems([...items, {
+      label: `option ${index}`,
+      key: index,
+      icon: <PieChartOutlined />,
+    }]);
+  };
 
   return (
-    <Button onClick={logout}>
-      Logout
-    </Button>
+    <div>
+      <div className={todoStyle.todo__wrapper}>
+        <div className={todoStyle.todo__left__pannel}>
+          <div className={todoStyle.todo__user__pannel}>
+            <Avatar size={46} icon={<UserOutlined />} />
+            <div>
+              <Title level={5}>User name</Title>
+              <Link onClick={logout}>Logout</Link>
+            </div>
+          </div>
+          <div className={todoStyle.todo__scroll}>
+            <div className={todoStyle.todo__projects}>
+              <div className={todoStyle.todo__projects__title}>
+                <Title level={3}>Projects</Title>
+                <Button type='text' onClick={createProject}>
+                  <PlusOutlined
+                    style={{ color: '#fff' }}
+                  />
+                </Button>
+              </div>
+              {items.length > 0
+                ? (
+                  <Menu
+                    defaultSelectedKeys={['1']}
+                    style={{ background: '#1a1a1a', border: 'none' }}
+                    items={items}
+                  />
+                )
+                : <Title level={4}>No created projects</Title>}
+            </div>
+          </div>
+        </div>
+        <div className={todoStyle.todo__center__pannel}>
+        </div>
+        <div className={todoStyle.todo__right__pannel}>Right menu</div>
+      </div>
+    </div>
   );
 };
 
